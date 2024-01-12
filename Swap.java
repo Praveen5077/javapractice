04f7e46a7b970b1706d61ce77fa3e9b1fd66fb31
Praveen Kumar R <113419335+Praveen5077@users.noreply.github.com>
2024-01-12 14:05:12 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,14 @@
+class Swap{
+public static void main(String[] args){
+int a,b,c;
+a=50;
+b=25;
+System.out.println("Before Swapping a:"+a);
+System.out.println("Before Swapping b:"+b);
+c=a;
+a=b;
+b=c;
+System.out.println("Swapping of a:"+a);
+System.out.println("Swapping of b:"+b);
+}
+}
---
+++ Swap2num.java
@@ -0,0 +1,11 @@
+class Swap2num{
+public static void main(String[] args){
+int a=10;
+int b=15;
+a=a+b;
+b=a-b;
+a=b-a;
+System.out.println("After Swapping a="+a);
+System.out.println("After Swapping b="+b);
+}
+}
